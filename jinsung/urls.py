169dafe335ae7b34c94fa7cf24f081a1890d25c4
ominyoung <ohmj090747@gmail.com>
2023-01-17 19:49:27 +0900

    css header,footer 분리 / 글쓰기 준비

--- jinsung/urls.py
+++ jinsung/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.print_page)
+    path("", views.index),
+    path("products/", views.products),
 ]
--- jinsung/views.py
+++ jinsung/views.py
@@ -4,5 +4,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
-def print_page(request):
-    return render(request, 'base.html')
+def index(request):
+    return render(request, 'jinsung/index.html')
+
+def products(request):
+    return render(request, 'jinsung/product.html')
