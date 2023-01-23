d08a6f428437a30b3af33bd55d378d1c5d6778b4
ominyoung <ohmj090747@gmail.com>
2023-01-24 00:12:49 +0900

    create post form

--- jinsung/views.py
+++ jinsung/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 from django.shortcuts import render
+from .forms import PostForm
+from .models import Post
 
 
 def index(request):
@@ -11,4 +13,10 @@ def products(request):
     return render(request, 'jinsung/product.html')
 
 def products_update(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            return redirect('/products/')
+    else:
+        form = PostForm()
     return render(request, 'jinsung/product_update.html')
