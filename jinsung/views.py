e50de5cba5c6af779cb8d90e65eace7c8eb513cc
ominyoung <ohmj090747@gmail.com>
2023-02-12 22:48:15 +0900

    태그에 따른 페이징 처리

--- jinsung/views.py
+++ jinsung/views.py
@@ -12,12 +12,13 @@ def index(request):
 
 def products(request):
     page = request.GET.get('page','1')
-    products = Post.objects.all()
+    products = Post.objects.get_queryset().order_by('id')
 
     if request.method == "POST": #검색기능
         search = request.POST.get('search')
         products = Post.objects.filter(title__contains = search)
     
+    # 페이징
     paginator = Paginator(products, 8)
     page_obj = paginator.get_page(page)
 
@@ -29,11 +30,23 @@ def products(request):
 
 def pro_tag(request):
     tag = request.GET.get('tag')
+    if tag == None:
+        tag = request.session['tag']
+    else:
+        request.session['tag'] = tag
+    
     products = Post.objects.filter(tag = tag)
+
+    # 페이징
+    page = request.GET.get('page','1')
+    paginator = Paginator(products, 8)
+    page_obj = paginator.get_page(page)
+
     context = {
-        'products':products,
+        'products':page_obj,
         'tags': Post.all_tags,
     }
+
     return render(request, 'jinsung/product.html',context)
 
 def products_update(request):
